ac8f2ee7c5eda7804e30206967fa634d01005f15
Ar9av <f2016731@pilani.bits-pilani.ac.in>
2024-11-03 23:08:03 +0530

    reload side bar

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -2,8 +2,9 @@
 import React, { useEffect, useState } from 'react';
 import { IoIosClose } from "react-icons/io";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
-import { Button, IconButton, Drawer, List, ListItem, ListItemText, ListItemIcon, Divider, Box, Typography } from '@mui/material';
+import { Button, IconButton, Drawer, List, ListItem, ListItemText, Divider, Box, Typography } from '@mui/material';
 import { BiChat } from "react-icons/bi";
+import { Refresh as RefreshIcon } from '@mui/icons-material';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useClerk } from '@clerk/clerk-react';
@@ -12,110 +13,118 @@ import { ThemeProvider } from '@mui/material/styles';
 import darkTheme from '../theme'; 
 
 const Sidebar = ({ isOpen, onClose }) => {
-  const [previousReports, setPreviousReports] = useState([]);
-  const router = useRouter();
-  const { session } = useClerk();
-  const pathname = usePathname();
+    const [previousReports, setPreviousReports] = useState([]);
+    const router = useRouter();
+    const { session } = useClerk();
+    const pathname = usePathname();
 
-  useEffect(() => {
     const fetchHashes = async () => {
-      if (session && !localStorage.getItem('previousReports')) {
-        const user_email = session.user.emailAddresses[0].emailAddress;
-        try {
-          const response = await fetch(`/api/getUserHashes?email=${encodeURIComponent(user_email)}`);
-          const data = await response.json();
-          setPreviousReports(data);
-          localStorage.setItem('previousReports', JSON.stringify(data));
-        } catch (error) {
-          console.error('Failed to fetch data:', error);
+        if (session) {
+            const user_email = session.user.emailAddresses[0].emailAddress;
+            try {
+                const response = await fetch(`/api/getUserHashes?email=${encodeURIComponent(user_email)}`);
+                const data = await response.json();
+                setPreviousReports(data);
+                localStorage.setItem('previousReports', JSON.stringify(data));
+            } catch (error) {
+                console.error('Failed to fetch data:', error);
+            }
         }
-      } else {
-        const cachedData = localStorage.getItem('previousReports');
-        if (cachedData) {
-          setPreviousReports(JSON.parse(cachedData));
-        }
-      }
     };
 
-    fetchHashes();
-  }, [session]);
+    useEffect(() => {
+        const cachedData = localStorage.getItem('previousReports');
+        if (cachedData) {
+            setPreviousReports(JSON.parse(cachedData));
+        } else {
+            fetchHashes();
+        }
+    }, [session]);
 
-  const handleHistoryClick = (text) => {
-    localStorage.setItem('searchParams', text);
-    if (pathname === '/search') {
-      window.location.reload();
-    } else {
-      router.push('/search');
-    }
-  };
+    const handleHistoryClick = (text) => {
+        localStorage.setItem('searchParams', text);
+        if (pathname === '/search') {
+            window.location.reload();
+        } else {
+            router.push('/search');
+        }
+    };
 
-  const formatTimestamp = (timestamp) => {
-    const now = new Date();
-    const timeDiff = now - new Date(timestamp);
+    const handleRefreshClick = () => {
+        localStorage.removeItem('previousReports');
+        fetchHashes();
+    };
 
-    const minutes = Math.floor(timeDiff / (1000 * 60));
-    if (minutes < 60) {
-      return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
-    }
+    const formatTimestamp = (timestamp) => {
+        const now = new Date();
+        const timeDiff = now - new Date(timestamp);
 
-    const hours = Math.floor(minutes / 60);
-    if (hours < 24) {
-      return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
-    }
+        const minutes = Math.floor(timeDiff / (1000 * 60));
+        if (minutes < 60) {
+            return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
+        }
 
-    const days = Math.floor(hours / 24);
-    if (days < 7) {
-      return days === 1 ? '1 day ago' : `${days} days ago`;
-    }
+        const hours = Math.floor(minutes / 60);
+        if (hours < 24) {
+            return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
+        }
 
-    return new Date(timestamp).toLocaleDateString();
-  };
+        const days = Math.floor(hours / 24);
+        if (days < 7) {
+            return days === 1 ? '1 day ago' : `${days} days ago`;
+        }
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-    <Drawer
-      anchor="left"
-      open={isOpen}
-      onClose={onClose}
-      PaperProps={{ style: { width: 250, backgroundColor: '#000', color: '#fff' } }}
-    >
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2, borderBottom: '1px solid #444' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-          <HiOutlineClipboardDocumentList size={30} />
-          <Typography variant="h6">Reports</Typography>
-        </Box>
-        <IconButton onClick={onClose} sx={{ color: '#fff' }}>
-          <IoIosClose size={24} />
-        </IconButton>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 2 }}>
-        <Link href='/create' passHref>
-          <Button variant="outlined" startIcon={<BiChat size={20} />} fullWidth>
-            New Report
-          </Button>
-        </Link>
-      </Box>
-      <Divider />
-      <Box sx={{ overflowY: 'auto', maxHeight: 'calc(100vh - 200px)' }}>
-        <List>
-          {previousReports.map((item, index) => (
-            <ListItem
-              button
-              key={index}
-              onClick={() => handleHistoryClick(item.payload)}
-              sx={{ bgcolor: '#27282A', my: 1, borderRadius: 2, '&:hover': { bgcolor: '#323335' } }}
+        return new Date(timestamp).toLocaleDateString();
+    };
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <Drawer
+                anchor="left"
+                open={isOpen}
+                onClose={onClose}
+                PaperProps={{ style: { width: 250, backgroundColor: '#000', color: '#fff' } }}
             >
-              <ListItemText
-                primary={<Typography variant="body2" noWrap>{item.title}</Typography>}
-                secondary={<Typography variant="caption" color="textSecondary">{formatTimestamp(item.created_at)}</Typography>}
-              />
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </Drawer>
-    </ThemeProvider >
-  );
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2, borderBottom: '1px solid #444' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                        <HiOutlineClipboardDocumentList size={30} />
+                        <Typography variant="h6">Reports</Typography>
+                    </Box>
+                    <IconButton onClick={onClose} sx={{ color: '#fff' }}>
+                        <IoIosClose size={24} />
+                    </IconButton>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 2, gap: 1 }}>
+                    <Link href='/create' passHref>
+                        <Button variant="outlined" startIcon={<BiChat size={20} />} fullWidth>
+                            New Report
+                        </Button>
+                    </Link>
+                    <IconButton onClick={handleRefreshClick} sx={{ color: '#fff' }}>
+                        <RefreshIcon />
+                    </IconButton>
+                </Box>
+                <Divider />
+                <Box sx={{ overflowY: 'auto', maxHeight: 'calc(100vh - 200px)' }}>
+                    <List>
+                        {previousReports.map((item, index) => (
+                            <ListItem
+                                button
+                                key={index}
+                                onClick={() => handleHistoryClick(item.payload)}
+                                sx={{ bgcolor: '#27282A', my: 1, borderRadius: 2, '&:hover': { bgcolor: '#323335' } }}
+                            >
+                                <ListItemText
+                                    primary={<Typography variant="body2" noWrap>{item.title}</Typography>}
+                                    secondary={<Typography variant="caption" color="textSecondary">{formatTimestamp(item.created_at)}</Typography>}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+        </ThemeProvider >
+    );
 };
 
 export default Sidebar;
